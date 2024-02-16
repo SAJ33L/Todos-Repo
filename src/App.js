c63d824dd3d20bba5c4e2a7af3d766c4d411d345
saj33l <sajeel.khan01@gmail.com>
2024-02-16 23:39:24 +0500

    random

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>
+        My App Works fine just get it done with!!!!!!!
+      </h3>
+      <ButtonIcon/>
     </div>
   );
 }
 
 export default App;
+
+
+function ButtonIcon(){
+  return (
+    <>
+    <button onClick={alert("Sajeel bhai rocks!!!!!")}>
+      click me bhai
+    </button>
+    </>
+  );
+}
